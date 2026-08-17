e049a9fbe57488cc719cf79ad7f3db00dc3f7a6f
KoishiMoe <68314080+KoishiMoe@users.noreply.github.com>
2026-08-17 02:04:09 +0000

    build: move to lolicode maven

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,8 +15,13 @@ dependencyResolutionManagement {
             content { includeGroupByRegex("com\\.github\\.walkyst\\..*") }
         }
         maven {
-            name = "Lolicode on Codeberg"
-            url = uri("https://codeberg.org/api/packages/lolicode/maven")
+            name = "Lolicode Releases"
+            url = uri("https://maven.lolicode.org/releases")
+            content { includeGroupByRegex("org\\.lolicode.*") }
+        }
+        maven {
+            name = "Lolicode Snapshots"
+            url = uri("https://maven.lolicode.org/snapshots")
             content { includeGroupByRegex("org\\.lolicode.*") }
         }
     }
